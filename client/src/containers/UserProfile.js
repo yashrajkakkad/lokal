f591fdd435e163fb1a7d483c035432db336fde8c
aman-dave <aman.d@ahduni.edu.in>
2021-03-27 21:55:04 +0530

    fix: added better stylings

--- client/src/containers/UserProfile.js
+++ client/src/containers/UserProfile.js
@@ -51,7 +51,7 @@ const styles = (theme) => ({
     height: "100vh",
     width: "100%",
     overflow: "auto",
-    backgroundColor: "#bac9fe",
+    backgroundColor: "white",
   },
   logoContainer: {
     textAlign: "center",
@@ -79,12 +79,26 @@ const styles = (theme) => ({
   },
   fieldKey: {
     marginTop: 15,
+    padding: 5,
+    borderTopRightRadius: 5,
+    borderTopLeftRadius: 5,
+    color: "white",
+    background:
+      "linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898",
+    backgroundBlendMode: "multiply,multiply",
   },
   fieldValue: {
     padding: 5,
-    backgroundColor: "#4a83fe",
+    backgroundColor: "#DCD9D4",
+    backgroundImage:
+      "linear-gradient(to bottom, rgba(255,255,255,0.50) 0%, rgba(0,0,0,0.50) 100%), radial-gradient(at 50% 0%, rgba(255,255,255,0.10) 0%, rgba(0,0,0,0.50) 50%)",
+    backgroundBlendMode: "soft-light,screen",
     boxShadow: "0 8px 6px -6px black",
-    border: "1px solid black",
+    borderBottomRightRadius: 5,
+    borderBottomLeftRadius: 5,
+  },
+  name: {
+    textAlign: "center",
   },
 });
 
@@ -93,13 +107,17 @@ const UserProfile = (props) => {
 
   return (
     <div className={classes.screen}>
-      <div className={classes.logoContainer}>
-        <img src={logo} alt="logo" className={classes.logo} />
-        {/* <div className={classes.userName}>John Doe</div> */}
-      </div>
       <UserHeader>
-        {user.firstName} {user.lastName}
+        <div className={classes.logoContainer}>
+          <img src={logo} alt="logo" className={classes.logo} />
+          {/* <div className={classes.userName}>John Doe</div> */}
+        </div>
       </UserHeader>
+
+      <h2 className={classes.name}>
+        {" "}
+        {user.firstName} {user.lastName}{" "}
+      </h2>
       <div className={classes.container}>
         {Object.entries(user).map(([key, value]) => {
           return (
@@ -109,21 +127,18 @@ const UserProfile = (props) => {
             </>
           );
         })}
+        <div>
+          <div className={classes.fieldKey}>Your recent transactions:</div>
 
-        <div className={classes.transactions}>
-          Your recent transactions:
-          <br />
-          <FormControl className={classes.formControl}>
-            <Select labelId="demo-simple-select-label" id="demo-simple-select">
-              {transactions.map((transaction) => {
-                return (
-                  <MenuItem className={classes.menuItem} value={10}>
-                    {transaction.amount} on date {transaction.date}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
+          <div style={{ width: "98%" }} className={classes.fieldValue}>
+            {transactions.map((transaction) => {
+              return (
+                <div>
+                  {transaction.amount} on date {transaction.date}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
